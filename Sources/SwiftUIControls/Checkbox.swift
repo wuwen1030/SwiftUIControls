faec36cc78672a33722a1c2f2875bd600e95fb7e
wuwen <wuwen.xb@alibaba-inc.com>
2019-10-23 10:47:28 +0800

    feat: Add checkbox back

---
+++ Sources/SwiftUIControls/Checkbox.swift
@@ -0,0 +1,20 @@
+//
+//  Checkbox.swift
+//  
+//
+//  Created by XIABIN on 2019/10/23.
+//
+
+import SwiftUI
+
+@Binding public var checked: Bool
+public var checkedImage: String
+public var uncheckedImage: String
+public var valueChanged: (() -> Void)?
+public var body: some View {
+    Image(checked ? checkedImage : uncheckedImage)
+        .onTapGesture {
+            self.checked.toggle()
+            self.valueChanged?()
+    }
+}
